468ccf09fc2e2348d420e9d4290bab938a4660c3
CesarArellano <raywayday@gmail.com>
2023-03-29 12:14:47 -0600

    Add new two programming problems

---
+++ find_median_in_arrays.dart
@@ -0,0 +1,23 @@
+void main() {
+  print(findMedianSortedArrays([1,4], [2,3,5]));
+}
+
+
+double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
+  int m = nums1.length;
+  int n = nums2.length;
+
+  if( m < 0 || n > 1000 ) return 0.0;
+  if( n < 0 || n > 1000 ) return 0.0;
+  if( m + n < 1 || m + 1 > 2000 ) return 0.0;
+
+  List<int> mergedList = [ ...nums1, ...nums2]..sort((a, b) => a.compareTo(b));
+
+  final len = mergedList.length;
+
+  if( len % 2 == 0 ) {
+    return (mergedList[ len ~/ 2  - 1 ] + mergedList[ len ~/ 2 ]) / 2;
+  }
+
+  return mergedList[len ~/ 2].toDouble();
+}
---
+++ longest_substring.dart
@@ -0,0 +1,52 @@
+void main() {
+  print(lengthOfLongestSubstring2('dvdfkppskdpjapsjfig'));
+}
+
+int lengthOfLongestSubstring(String s) {
+  int maxLength = 0;
+  int msgLength = s.length;
+  List<String> letterList = [];
+  if( msgLength < 0 || msgLength > 500000 ) return 0;
+
+  for( int i = 0; i < msgLength; i++ ) {
+    final letter = s[i];
+
+    if( !letterList.contains(letter) ) {
+      letterList = [ ...letterList, letter ];
+      if( i + 1 == msgLength ) {
+        maxLength = maxLength > letterList.length ? maxLength : letterList.length;
+      }
+      continue;
+    }
+
+    maxLength = maxLength > letterList.length ? maxLength : letterList.length;
+    letterList = [ letter ];
+  }
+
+  return maxLength;
+}
+
+int lengthOfLongestSubstring2(String s) {
+  // Initialize variables
+  int maxLength = 0;
+  int left = 0;
+  Map<String, int> charIndexMap = {};
+
+  // Iterate through the string
+  for (int right = 0; right < s.length; right++) {
+    String currentChar = s[right];
+
+    // If the character has been seen before, update the left index
+    if (charIndexMap.containsKey(currentChar) && charIndexMap[currentChar]! >= left) {
+      left = charIndexMap[currentChar]! + 1;
+    }
+
+    // Update the character index in the map
+    charIndexMap[currentChar] = right;
+
+    // Update the maximum length if necessary
+    maxLength = maxLength > right - left + 1 ? maxLength : right - left + 1;
+  }
+
+  return maxLength;
+}
